aa2e912f610a08c240e7c9f4831d9141f558df7a
yanghaizhu <252489667@qq.com>
2024-06-04 23:27:43 +0800

    [task] add uuid, searchbyuuid, relationshiplist using uuid,and link of uuid

--- app/App/stringHandler.py
+++ app/App/stringHandler.py
@@ -1,5 +1,7 @@
 from flask import Blueprint,request,redirect, current_app
+import json
 
+import markdown
 import os
 def str_upper_split_to_list(string):
     l = [s.strip().upper() for s in string.split(",")]
@@ -11,4 +13,23 @@ def search_revers_action_flag(string):
     current_app.log.debug(string)
     return string.startswith("-") or string.startswith("!")
     
-    
+def markdownToHtmlWithExtensions(mdString):
+    htmlString = markdown.markdown(
+        mdString,
+        extensions=[
+            'tables',
+            'markdown.extensions.attr_list',
+            'markdown.extensions.def_list',
+            'markdown.extensions.fenced_code',
+            'markdown.extensions.footnotes',
+            'markdown.extensions.codehilite',
+            "mdx_math"
+        ],
+        extension_configs={
+            'mdx_math': {
+                'enable_dollar_delimiter': True,  # 是否启用单美元符号（默认只启用双美元）
+                'add_preview': True  # 在公式加载成功前是否启用预览（默认不启用）
+            }
+        }
+    )
+    return htmlString
--- app/App/viewsHome.py
+++ app/App/viewsHome.py
@@ -3,7 +3,8 @@ from  flask import Flask, render_template, request, url_for, redirect,make_respo
 from  wtforms import StringField, PasswordField, SubmitField
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, EqualTo
-
+import uuid
+#str(uuid.uuid4()) to generate random string, used as unique string. like id.
 
 #from .models import *
 import json
@@ -21,7 +22,6 @@ from .actionHandler import *
 # blueprint is used for route. "todo" name can be others. todoBlueprint would be used at create_app, in __init__.py. 
 # We can have more blueprints, with different name, and at create_app side, register them one by one.
 homeBlueprint = Blueprint('home', __name__)
-
 
 @homeBlueprint.route('/find/<string:findStrings>/',methods=['GET','POST'])
 # look up from All files, collect item content into a List.
@@ -92,19 +92,7 @@ def showFile(file):
         logger.debug(item["keyList"])
         itemTmp = item
         itemTmp["file"] = f
-        itemTmp["detail_html"] = markdown.markdown(itemTmp["detail"],extensions=['tables',
-                                                                            'markdown.extensions.attr_list',
-                                                                            'markdown.extensions.def_list',
-                                                                            'markdown.extensions.fenced_code',
-                                                                            'markdown.extensions.footnotes',
-                                                                            'markdown.extensions.codehilite',
-                                                                            "mdx_math"],
-                                                                extension_configs={
-                                                                            'mdx_math': {
-                                                                                'enable_dollar_delimiter': True,  # 是否启用单美元符号（默认只启用双美元）
-                                                                                'add_preview': True  # 在公式加载成功前是否启用预览（默认不启用）
-                                                                            }
-                                                                        })
+        itemTmp["detail_html"] = markdownToHtmlWithExtensions(itemTmp["detail"])
         print(itemTmp["detail"])
         itemList.append(itemTmp)
 
@@ -112,7 +100,6 @@ def showFile(file):
 
     return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list)
     
-        
         
 @homeBlueprint.route('/form/<string:file>/<int:idx>/',methods=['GET','POST'])
 # look up from All files, collect item content into a List.
@@ -137,50 +124,9 @@ def formModify(file,idx):
         if idx == item["id"]:
             itemTmp = item
             itemTmp["file"] = f
-            itemTmp["detail_html"] = markdown.markdown(itemTmp["detail"],extensions=['tables',
-                                                                                'markdown.extensions.attr_list',
-                                                                                'markdown.extensions.def_list',
-                                                                                'markdown.extensions.fenced_code',
-                                                                                'markdown.extensions.footnotes',
-                                                                                'markdown.extensions.codehilite',
-                                                                                "mdx_math"],
-                                                                    extension_configs={
-                                                                                'mdx_math': {
-                                                                                    'enable_dollar_delimiter': True,  # 是否启用单美元符号（默认只启用双美元）
-                                                                                    'add_preview': True  # 在公式加载成功前是否启用预览（默认不启用）
-                                                                                }
-                                                                            })
+            itemTmp["detail_html"] = markdownToHtmlWithExtensions(itemTmp["detail"])
             return  render_template('ModifyForm.html', file_list=file_list, item=itemTmp)
 
-@homeBlueprint.route('/form/new/',methods=['GET','POST'])
-# look up from All files, collect item content into a List.
-# 遍历所有的json文件，找到关键字对于的内容列表
-def formAdd():
-    logger = current_app.log
-    to_file = ""
-    itemList = []
-    findStr = ""
-    file_list = []
-    
-    ### assumed only one dir.
-    file_list = get_json_file_list()
-
-    return  render_template('AddForm.html', file_list = file_list, to_file = to_file)
-
-@homeBlueprint.route('/form/new/<string:to_file>/',methods=['GET','POST'])
-# look up from All files, collect item content into a List.
-# 遍历所有的json文件，找到关键字对于的内容列表
-def formAddToFile(to_file):
-    logger = current_app.log
-    
-    itemList = []
-    findStr = ""
-    file_list = []
-    
-    ### assumed only one dir.
-    file_list = get_json_file_list()
-
-    return  render_template('AddForm.html', file_list=file_list,to_file = to_file)
 
     
 @homeBlueprint.route('/modify/<string:file>/<int:idx>/',methods=['POST'])
@@ -211,19 +157,14 @@ def modify(file,idx):
             itemModify["keyList"].append(k.upper().strip())
         itemModify["keyList"] = list(filter(None,itemModify["keyList"]))
         
-        itemModify["detail_html"] = markdown.markdown(itemModify["detail"],extensions=['tables',
-                                                                            'markdown.extensions.attr_list',
-                                                                            'markdown.extensions.def_list',
-                                                                            'markdown.extensions.fenced_code',
-                                                                            'markdown.extensions.footnotes',
-                                                                            'markdown.extensions.codehilite',
-                                                                            "mdx_math"],
-                                                                extension_configs={
-                                                                            'mdx_math': {
-                                                                                'enable_dollar_delimiter': True,  # 是否启用单美元符号（默认只启用双美元）
-                                                                                'add_preview': True  # 在公式加载成功前是否启用预览（默认不启用）
-                                                                            }
-                                                                        })
+        relationList = itemModify["relationList"].split(",")
+        print(relationList)
+        itemModify["relationList"] = []
+        for k in relationList:
+            itemModify["relationList"].append(k.strip())
+        itemModify["relationList"] = list(filter(None,itemModify["relationList"]))
+        
+        itemModify["detail_html"] = markdownToHtmlWithExtensions(itemModify["detail"])
         print(itemModify["detail_html"])
     else:
         return "not support get request !!!"
@@ -248,50 +189,6 @@ def modify(file,idx):
     im["file"] = newFile;
     return  render_template('ModifyForm.html', file_list=file_list, item=im)
     
-    
-@homeBlueprint.route('/add/',methods=['GET','POST'])
-# look up from All files, collect item content into a List.
-# 遍历所有的json文件，找到关键字对于的内容列表
-def add():
-    logger = current_app.log
-    file_list = []
-    newFile = ""
-    recordFile = "record"
-    record = dict()
-    record = record_load_from_file()
-    ### assumed only one dir.
-    file_list = get_json_file_list()
-            
-    itemAdd = dict()
-    if request.method=='POST':
-        for key in request.form:
-            print(key)
-            if key != "file":
-                itemAdd[key] = request.form[key]
-            else:
-                newFile = request.form[key]
-        itemAdd["id"] = record["idx"]
-        record["idx"] = record["idx"] + 1
-        keyList = itemAdd["keyList"].split(",")
-        print(keyList)
-        itemAdd["keyList"] = []
-        for k in keyList:
-            itemAdd["keyList"].append(k.upper().strip())
-            
-    itemListInJson = json_load_from_file(newFile)
-    
-    logger.critical("add item in file:"+newFile+",id:"+str(record["idx"]-1))
-
-        ## 增加item.
-    itemListInJson.append(itemAdd)
-    item = itemAdd
-    item["file"] = newFile
-        
-    json_dump_to_file(newFile,itemListInJson)
-    record_dump_to_file(record)
-         
-    return  render_template('ModifyForm.html', file_list=file_list, item=item)
-
 
 @homeBlueprint.route('/refresh/record/',methods=['GET','POST'])
 # look up from All files, collect item content into a List.
@@ -326,3 +223,135 @@ def refresh():
     logger.critical("refresh id in files, number:"+str(record["idx"]))
 
     return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list)
+
+
+
+
+@homeBlueprint.route('/',methods=['GET'])
+def index():
+    logger = current_app.log
+    ## collect file name into a list.
+    ## 文件名列表
+    file_list = []
+    
+    itemList = []
+    findStr = ""
+
+
+    logger.debug(itemList)
+
+    return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list)
+    
+        
+
+    
+@homeBlueprint.route('/add/',methods=['GET','POST'])
+# look up from All files, collect item content into a List.
+# 遍历所有的json文件，找到关键字对于的内容列表
+def add():
+    logger = current_app.log
+    file_list = []
+    newFile = ""
+    recordFile = "record"
+    record = dict()
+    record = record_load_from_file()
+    ### assumed only one dir.
+    file_list = get_json_file_list()
+            
+    itemAdd = dict()
+    if request.method=='POST':
+        for key in request.form:
+            print(key)
+            if key != "file":
+                itemAdd[key] = request.form[key]
+            else:
+                newFile = request.form[key]
+        itemAdd["id"] = record["idx"]
+        record["idx"] = record["idx"] + 1
+        
+        keyList = list(filter(None,re.split(",",itemAdd["keyList"])))
+        relationList = list(filter(None,re.split(",",itemAdd["relationList"])))
+        itemAdd["keyList"] = []
+        itemAdd["relationList"] = []
+        for k in keyList:
+            itemAdd["keyList"].append(k.upper().strip())
+        for r in relationList:
+            itemAdd["relationList"].append(r.strip())
+        itemAdd["uuid"] = str(uuid.uuid4()).replace('-','')
+            
+            
+    itemListInJson = json_load_from_file(newFile)
+    
+    logger.critical("add item in file:"+newFile+",id:"+str(record["idx"]-1))
+
+        ## 增加item.
+    itemListInJson.append(itemAdd)
+    item = itemAdd
+    item["file"] = newFile
+        
+    json_dump_to_file(newFile,itemListInJson)
+    record_dump_to_file(record)
+         
+    return  render_template('ModifyForm.html', file_list=file_list, item=item)
+
+
+
+@homeBlueprint.route('/form/new/',methods=['GET','POST'])
+# look up from All files, collect item content into a List.
+# 遍历所有的json文件，找到关键字对于的内容列表
+def formAdd():
+    logger = current_app.log
+    to_file = ""
+    itemList = []
+    findStr = ""
+    file_list = []
+    
+    ### assumed only one dir.
+    file_list = get_json_file_list()
+
+    return  render_template('AddForm.html', file_list = file_list, to_file = to_file)
+
+
+@homeBlueprint.route('/form/new/<string:to_file>/',methods=['GET','POST'])
+# look up from All files, collect item content into a List.
+# 遍历所有的json文件，找到关键字对于的内容列表
+def formAddToFile(to_file):
+    logger = current_app.log
+    
+    itemList = []
+    findStr = ""
+    file_list = []
+    
+    ### assumed only one dir.
+    file_list = get_json_file_list()
+
+    return  render_template('AddForm.html', file_list=file_list,to_file = to_file)
+
+@homeBlueprint.route('/uuid/<string:uuid>/',methods=['GET'])
+# look up from All files, collect item content into a List.
+# 遍历所有的json文件，找到关键字对于的内容列表
+def searchByuuid(uuid):
+    logger = current_app.log
+    file_list = []
+    
+            
+    logger = current_app.log
+    findStr = ""
+                
+    ## collect file name into a list.
+    ## 文件名列表
+    file_list = get_json_file_list()   
+    im = dict()
+    for f in file_list:
+        ## read json from json file.
+        ### This file path should be based on root path.  becasue it's just included by app.py, which is in root path. 
+        itemListInJson = json_load_from_file(f)
+        print(f)
+        ## 遍历所有item.
+        for item in itemListInJson:
+            if uuid == item["uuid"]:
+                im = item
+                im["file"] = f
+                return  render_template('ModifyForm.html', file_list=file_list, item=im)
+
+    return  render_template('ModifyForm.html', file_list=file_list, item=im)    
