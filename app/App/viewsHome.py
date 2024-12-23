8a3e08763a7e746cf0d59fef02d3152487551262
yanghaizhu <252489667@qq.com>
2024-12-23 15:44:34 +0800

    [task] new links include total links and showed content related links

--- app/App/viewsHome.py
+++ app/App/viewsHome.py
@@ -27,6 +27,8 @@ homeBlueprint = Blueprint('home', __name__)
 # look up from All files, collect item content into a List.
 # 遍历所有的json文件,遍历字典，找到关键字对于的内容列表
 def findListByItem(findStrings):
+
+    totalList=[]
     logger = current_app.log
     findStr = findStrings
     actionStr = "get"
@@ -55,6 +57,10 @@ def findListByItem(findStrings):
         itemListInJson = json_load_from_file(f)
         ## 遍历所有item.
         for k,item in itemListInJson.items():
+            linkItem = dict()
+            linkItem["title"]=item["title"]
+            linkItem["uuid"]=item["uuid"]
+            totalList.append(linkItem)
             allConditionMeet = True
             for search in search_key_list:
                 if actionStr == "xxx":
@@ -68,13 +74,14 @@ def findListByItem(findStrings):
                 itemTmp["file"] = f
                 itemTmp["detail_html"] = markdownToHtmlWithExtensions(itemTmp["detail"])
                 itemList.append(itemTmp)
-
-    return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list)
+    return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list, totalList=totalList)
     
 
 @homeBlueprint.route('/show/<string:file>/',methods=['GET'])
 # 整个文件里所有的内容list都显示出来。
 def showFile(file):
+    totalList=[]
+
     logger = current_app.log
     logger.critical("show file:"+file)
     ## collect file name into a list.
@@ -99,6 +106,10 @@ def showFile(file):
     itemListInJson = json_load_from_file(f)
     ## 遍历所有item.
     for k,item in itemListInJson.items():
+        linkItem = dict()
+        linkItem["title"]=item["title"]
+        linkItem["uuid"]=item["uuid"]
+        totalList.append(linkItem)
         logger.debug(item)
         logger.debug(findStr)
         logger.debug(item["keyList"])
@@ -109,7 +120,7 @@ def showFile(file):
 
     logger.debug(itemList)
 
-    return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list)
+    return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list, totalList=totalList)
     
 @homeBlueprint.route('/form/<string:file>/<string:uuid_str>/',methods=['GET','POST'])
 # look up from All files, collect item content into a List.
@@ -208,6 +219,8 @@ def modify(file,uuid_str):
 # look up from All files, collect item content into a List.
 # 遍历所有的json文件，找到关键字对于的内容列表
 def refresh():
+    totalList=[]
+
     logger = current_app.log
     file_list = []
     itemList = []
@@ -232,19 +245,25 @@ def refresh():
             itemListInJson[i]["id"] = record["idx"]
             record["idx"] += 1
             logger.debug(record["idx"])
+            linkItem = dict()
+            linkItem["title"]=itemListInJson[i]["title"]
+            linkItem["uuid"]=itemListInJson[i]["uuid"]
+            totalList.append(linkItem)
             
         json_dump_to_file(f, itemListInJson)
     record_dump_to_file(record)
 
     logger.critical("refresh id in files, number:"+str(record["idx"]))
 
-    return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list)
+    return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list, totalList=totalList)
 
 
 @homeBlueprint.route('/refresh/uuidTitle/',methods=['GET'])
 # look up from All files, collect item content into a List.
 # 遍历所有的json文件，找到关键字对于的内容列表
 def refreshuuidTitle():
+    totalList=[]
+
     logger = current_app.log
     file_list = []
     itemList = []
@@ -264,16 +283,22 @@ def refreshuuidTitle():
         for i in range(len(itemListInJson)):
             logger.debug(i)
             uuid_to_title[itemListInJson[i]["uuid"]] = itemListInJson[i]["title"]
-            
+
+            linkItem = dict()
+            linkItem["title"]=itemListInJson[i]["title"]
+            linkItem["uuid"]=itemListInJson[i]["uuid"]
+            totalList.append(linkItem)
     uuidTitle_dump_to_file(uuid_to_title)
 
 
-    return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list)
+    return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list, totalList=totalList)
 
 
 
 @homeBlueprint.route('/',methods=['GET'])
 def index():
+    totalList=[]
+
     logger = current_app.log
     ## collect file name into a list.
     ## 文件名列表
@@ -288,7 +313,7 @@ def index():
 
     logger.debug(itemList)
 
-    return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list)
+    return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list, totalList=totalList)
     
         
 
@@ -381,6 +406,7 @@ def formAddToFile(to_file):
 # look up from All files, collect item content into a List.
 # 遍历所有的json文件，找到关键字对于的内容列表
 def searchByuuid(uuid_str):
+    totalList=[]
 
     logger = current_app.log
     file_list = []
@@ -402,14 +428,21 @@ def searchByuuid(uuid_str):
         ### This file path should be based on root path.  becasue it's just included by app.py, which is in root path. 
         itemListInJson = json_load_from_file(f)
         ## 遍历所有item.
+
+        for k, item in itemListInJson.items():
+            linkItem = dict()
+            linkItem["title"] = item["title"]
+            linkItem["uuid"] = item["uuid"]
+            totalList.append(linkItem)
+
         item = itemListInJson[uuid_str]
         im = item
         im["file"] = f
         im["detail_html"] = markdownToHtmlWithExtensions(im["detail"])
         itemList.append(im)
-        return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list)
+        return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list, totalList=totalList)
 
-    return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list)
+    return  render_template('home.html', findStr=findStr, itemList=itemList, file_list=file_list, totalList=totalList)
 
 
 @homeBlueprint.route('/update/', methods=['POST'])
